3461088e8d55c23b55a2216fd96aa73faabadbab
RepoByDhruv <dhruvgoel1100@gmail.com>
2024-05-08 13:21:37 +0530

    first commit

---
+++ 01_basic/01_basic.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        (&) AND OPERATION
+
+//        int x=0b1010;
+//        int y=0b0110;
+//        int z;
+//        z=x&y;
+//        System.out.println(z);
+
+//        (|) OR OPERATION
+
+//        int x=10;
+//        int y=6;
+//        int z;
+//        z=x|y;
+//        System.out.println(z);
+
+
+//        (^) XOR OPERATION
+//
+//    int x = 10;
+//    int y = 6;
+//    int z;
+//    z=x^y;
+//        System.out.println(z);
+
+//        left and right shift operations
+////
+       int x = 10;
+       int y ;
+       y=>>1;
+       System.out.println(y);
+////
+////    }
+//    }
+//    
