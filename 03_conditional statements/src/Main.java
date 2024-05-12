843dc9c83752608fe7c005edacd6d31e2dea5d79
RepoByDhruv <dhruvgoel1100@gmail.com>
2024-05-12 21:25:59 +0530

    3 and 4th file

---
+++ 03_conditional statements/src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        relational operators they give only boolean result
+//
+//        int a = 5, b = 10, c = 15;
+//        System.out.println(a < b);
+//
+//
+////        logical operators they are used to compare two conditions
+//
+//        int x = 5, y = 10, z = 15;
+//        System.out.println(x < y || x > z);
+//
+//
+//         conditional statements
+//
+//
+//        int n = -5;
+//        if (n >= 0) {
+//            System.out.println("possitive");
+//        } else {
+//            System.out.println("negative");
+//
+//            multiple if else conditions
+//        }
+
+//        ONE PRACTICE QUESTION
+
+//        int p=10 , q=20 , r=30;
+//        if(r>p && r<q)
+//        {
+//            System.out.println(r);
+//        }
+//        else
+//        {
+//            if(q<p || p<q){
+//                System.out.println(q);
+//            }else {
+//                System.out.println(p);
+//            }
+//        }
+
+
+
+    }
+}
---
+++ 04_Practice of conditional statements/src/Main.java
@@ -0,0 +1,94 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+//        int n;
+//        System.out.println("Enter a number here");
+//        Scanner sc = new Scanner(System.in);
+//          n = sc.nextInt();
+//        if(n%2==0) {
+//            System.out.println("even number");
+//        }else {
+//            System.out.println("odd number");
+//        }
+//
+//
+//
+//        int a;
+//        System.out.println("Enter the age here");
+//        Scanner sc = new Scanner(System.in);
+//        a = sc.nextInt();
+//        if(a>=18)
+//        {
+//            System.out.println("ADULT");
+//        }
+//        else
+//        {
+//            System.out.println("NOT ADULT");
+//        }
+//
+//
+//        int m1, m2, m3;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter marks of three subjects");
+//
+//
+//        m1 = sc.nextInt();
+//        m2 = sc.nextInt();
+//        m3 = sc.nextInt();
+//
+//        float avg = (float) (m1 + m2 + m3) / 3;
+//
+//        if (avg > 70) {
+//            System.out.println("Grade A");
+//        } else if (avg >= 70 && avg < 70) {
+//            System.out.println("Grade b");
+//        } else if (avg >= 50 && avg < 60) {
+//            System.out.println("Grade C");
+//        } else if (avg >= 40 && avg < 50) {
+//            System.out.println("Grade D");
+//        } else {
+//            System.out.println("Grade E");
+//
+//        }
+
+//        FIND RADIX OF A NUMBER GIVEN IN A STRING
+//     done --
+//
+//
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the day number");
+//        int day = sc.nextInt();
+//        if(day==1)
+//        {
+//            System.out.println("Monday");
+//        }
+//        else if(day==2)
+//        {
+//            System.out.println("Tuesday");
+//        }
+//        else if(day==3)
+//        {
+//            System.out.println("Wednesday");
+//        }
+//        else if(day==4)
+//        {
+//            System.out.println("thrusday");
+//        }
+//        else if(day==5)
+//        {
+//            System.out.println("friday");
+//        } else if(day==6)
+//        {
+//            System.out.println("saturday");
+//        }
+//        else if(day==7)
+//        {
+//            System.out.println("sunday");
+//        }
+//      else {
+//            System.out.println("invalid day");
+//        }
+
+
+    }
+}
