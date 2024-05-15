a1b86539a56fc75cb63ac74858c578e9d43528d2
RepoByDhruv <dhruvgoel1100@gmail.com>
2024-05-15 10:55:22 +0530

    loops intro

---
+++ Loops by abdul bari/src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+//        int i = 1;
+//
+//        while (i < 100) {
+//            System.out.println(i);
+//            i = i * 2;
+//        }
+//
+//        int j = 2;
+//
+//        while (j < 20) {
+//            System.out.println(j);
+//            j = j * 2;
+//        }
+//
+//
+//        int k = 1;
+//        do {
+//            System.out.println(k);
+//            k = k * 2;
+//        } while(k<100);
+
+
+
+
+//        USING FOR LOOPS
+
+
+//        int j=0;
+//        for( ; j<=10; j++)
+//        {
+//            System.out.println(j);
+//        }
+
+
+
+    }
+}
