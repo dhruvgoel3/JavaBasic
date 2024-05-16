6c86a340cdf18ee081d0daa9e754ed4ec38f36ce
RepoByDhruv <dhruvgoel1100@gmail.com>
2024-05-16 11:50:20 +0530

    loops practice

---
+++ 06_Practice of loops (1) abdul bari/src/Main.java
@@ -0,0 +1,126 @@
+import java.sql.SQLOutput;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+//        Multiplication table of 5
+//
+//    Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a Number");
+//   int n = sc.nextInt();
+//
+//   for (int i=1; i<=10;i++)
+//  {
+//    System.out.println(n+"x"+i+"="+n*i);
+//      }
+
+//       multiplication table of 50
+
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a Number");
+//        int n = sc.nextInt();
+
+//        for(int i = 1; i<=10; i++)
+//        {
+//            System.out.println(n+" x "+i+'='+n*i);
+//        }
+
+//    sum of first n natural numbers
+//
+//         Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter s number to get sum");
+//       int n = sc.nextInt();
+//        int sum = 0;
+//         for(int i = 1; i<=n; i++)
+//{
+//     sum = sum+i;
+//}
+//        System.out.println("Sum of "+n+" number is "+sum);
+
+//        |
+//        |
+//        |
+//        |
+//        FACTORIAL CONCEPT
+//
+//   Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter any number here");
+//        int n = sc.nextInt();
+//
+//   long factorial = 1;
+//   for(int i = 1; i<=n; i++ )
+//   {
+//       factorial = factorial*i;
+//
+//   }
+//        System.out.println("factorial is "+factorial);
+
+//        |
+//        |
+//        |
+//        |
+//        |
+//          TILL NOW WE HAVE DONE THREE BASIC QUESTIONS IN JAVA
+//             NOW ITS TIME TO DO THOSE THREE QUESTIONS AGAIN
+//        |
+//        |
+//        |
+//        |
+
+
+//     ______________(1. Multiplication table)______________
+
+
+//
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a number here");
+//        int n = sc.nextInt();
+//
+//        for(int i = 1; i<=20; i++)
+//        {
+//            System.out.println(n+"x"+i+"="+n*i);
+//        }
+
+
+
+//        _____________(2. SUM OF FIRST N NATURAL NUMBERS)____________
+//
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("ENTER A NUMBER HERE TO GDT SUM");
+//        int n = sc.nextInt();
+//        int sum = 0;
+//        for(int i = 1; i<=n; i++)
+//        {
+//            sum = sum+i;
+//        }
+//        System.out.println("sum of numbers is "+sum);
+
+
+
+//        ___________________(3. factorials)___________________
+//
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("enter a number here");
+//        int n = sc.nextInt();
+//
+//        long factorial = 1;
+//
+//        for(int i = 1; i<=n; i++)
+//        {
+//            factorial = factorial*i;
+//
+//        }
+//        System.out.println("factorial of "+n+" is "+factorial);
+
+
+
+
+
+
+
+    }
+
+
+
+    }
