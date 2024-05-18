f6fab3d5d8ddc910a3518759a56a632d56dc390a
RepoByDhruv <dhruvgoel1100@gmail.com>
2024-05-18 21:21:44 +0530

    Array starting

---
+++ 0_8 Array starting/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+
+//      ______ PRACTICING OF 1D ARRAY______
+
+
+//        int A[]=new int[10];
+//        int B[]={1,2,3,4,5};
+//        System.out.println(B.length);
+
+
+//       ___________FINDING SUM OF ALL ELEMENTS_______
+
+        int A[]= {1,4,3,7,5,7,8,5,7};
+        int sum = 0;
+        for(int i = 0;i<A.length; i++)
+        {
+            sum = sum +A[i];
+        }
+        System.out.println(sum);
+
+
+//        ___________SEARCHING AN ELEMENT_________
+
+
+
+
+
+        
+    }
+}
