0bbe748997fcec37e8c3053c871463c4d22861a8
RepoByDhruv <dhruvgoel1100@gmail.com>
2024-05-21 22:09:56 +0530

    methods in java

---
+++ methods in java 1/src/Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    static int max(int x , int y)
+    {
+        if(x>y)
+            return x;
+        else
+            return y;
+    }
+    public static void main(String[] args)
+    {
+int a = 10 , b = 15;
+        System.out.println(max(a,b));
+    }
+}
