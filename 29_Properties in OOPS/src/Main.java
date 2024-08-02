4474627c81cee70b727a51f7774473d547aed4fa
RepoByDhruv <dhruvgoel1100@gmail.com>
2024-08-02 23:37:44 +0530

    Properties

---
+++ 29_Properties in OOPS/src/Main.java
@@ -0,0 +1,17 @@
+class student
+{
+//READ ONLY , WRITE ONLY , READ AND WRITABLE ONLY
+
+private int roll;
+public int getRollNumber()
+{
+    return roll;
+}
+
+}
+public class Main {
+    public static void main(String[] args)
+    {
+
+    }
+}
